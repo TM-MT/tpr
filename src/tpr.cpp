b94b857ccea60e1631d97ddcb0af5d1c8fa051a4
TM-MT <tm-east@outlook.com>
2021-06-03 15:22:00 +0900

    update TPR  - [solve()] divide by stages, not by phase  - fix allocation for new_x
    fix PMlib report format

--- src/tpr.cpp
+++ src/tpr.cpp
@@ -9,62 +9,63 @@
 
 
 int TPR::solve() {
-    tpr_forward();
-    tpr_backward();
+    // tpr_forward();
+    for (int st = 0; st < this->n; st += s) {
+        tpr_stage1(st, st + s - 1);
+    }
 
+    tpr_stage2();
+
+    // tpr_backward();
+    for (int st = 0; st < this->n; st += s) {
+        tpr_stage3(st, st + s - 1);
+    }
     return 0;
 }
 
+
 /**
- * @brief      TPR FORWARD REDUCTION
+ * @brief      TPR STAGE 1
+ *
+ * @param[in]  st     start index of equation that this function calculate
+ * @param[in]  ed     end index of equation that this function calculate
  */
-void TPR::tpr_forward() {
-    int n = this->n;
-    int s = this->s;
+void TPR::tpr_stage1(int st, int ed) {
+    mk_bkup_init(st, ed);
 
-    int k, u, p;
-    
-    mk_bkup_init();
+    for (int k = 1; k <= fllog2(s); k += 1) {
+        int u = pow2(k-1);
 
-    // STAGE 1
-    for (k = 1; k <= fllog2(s); k += 1) {
-        u = pow2(k-1);
-        p = 0;
+        EquationInfo eqbuff[s];
+        int j = 0;  // j <= s
 
-        while (p < n) {
-            EquationInfo eqbuff[s];
-            int j = 0;  // j <= s
-    
-            for (int i = p; i < p + s; i += pow2(k)) {
-                eqbuff[j] = update_section(i, u);
-                j += 1;
-            }
-            // p >= 0, k >= 1 -> i >= 0
-            for (int i=p+pow2(k)-1; i < p+s; i += pow2(k)) {
-                eqbuff[j] = update_section(i, u);
-                j += 1;
-            }
+        for (int i = st; i <= ed; i += pow2(k)) {
+            eqbuff[j] = update_section(i, u);
+            j += 1;
+        }
+        // ed >= 0, k >= 1 -> i >= 0
+        for (int i=st+pow2(k)-1; i <= ed; i += pow2(k)) {
+            eqbuff[j] = update_section(i, u);
+            j += 1;
+        }
 
-            assert(j <= s);
-            for (int i = 0; i < j; i++) {
-                patch_equation_info(eqbuff[i]);
-            }
-
-            p += s;
+        assert(j <= s);
+        for (int i = 0; i < j; i++) {
+            patch_equation_info(eqbuff[i]);
         }
     }
 
-    // k = k + 1;
-    u = pow2(k - 1);
-    p = 0;
-    while (p < n) {
-        EquationInfo eqi = update_uppper_no_check(p, p + (u - 1));
-        patch_equation_info(eqi);
-        p = p + s;
-    }
+    EquationInfo eqi = update_uppper_no_check(st, ed);
+    patch_equation_info(eqi);
 
-    mk_bkup_st1();
+    mk_bkup_st1(st, ed);
+}
 
+/**
+ * @brief TPR STAGE 2
+ *
+ */
+void TPR::tpr_stage2() {
     // INTERMIDIATE STAGE
     {
         EquationInfo eqbuff[n / s];
@@ -81,29 +82,110 @@ void TPR::tpr_forward() {
         }
     }
 
-    
-    // STAGE 2
-    int j = 0;
-    while ( j < (int)log2((double)n / s) - 1) {
-        u = pow2(k-1);
 
-        EquationInfo eqbuff[n / s];
-        int idx = 0;
-        for (int i = pow2(k)-1; i < n; i += pow2(k)) {
-            eqbuff[idx] = update_global(i, u);
-            idx += 1;
+    // STAGE 2 FR (CR FORWARD REDUCTION)
+    {
+
+        // int j = 0;
+        // while ( j < (int)log2((double)n / s) - 1) {
+        for (int j = 0, k = log2(s); j < (int)log2((double)n / s) - 1; j++, k++) {
+            int u = pow2(k-1);
+
+            EquationInfo eqbuff[n / s];
+            int idx = 0;
+            for (int i = pow2(k)-1; i < n; i += pow2(k)) {
+                eqbuff[idx] = update_global(i, u);
+                idx += 1;
+            }
+            // j += 1;
+            // k += 1;
+
+            for (int i = 0; i < idx; i++) {
+                patch_equation_info(eqbuff[i]);
+            }
         }
-        j += 1;
-        k += 1;
+    }
 
-        for (int i = 0; i < idx; i++) {
-            patch_equation_info(eqbuff[i]);
+
+    int capital_i = n / 2;
+    int m = n / s;
+
+    int u = capital_i;
+    int i = capital_i-1;
+
+    // CR BACKWARD SUBSTITUTION STEP 1
+    {
+        real inv_det = 1.0 / (diag[i+u]*diag[i] - c[i]*a[i+u]);
+
+        x[i] = (diag[i+u]*rhs[i] - c[i]*rhs[i+u]) * inv_det;
+        x[i+u] =  (rhs[i+u]*diag[i] - rhs[i]*a[i+u]) * inv_det;
+    }
+
+    // CR BACKWARD SUBSTITUTION
+    {
+        int j = 0;
+        while (j < fllog2(m) - 1) {
+            capital_i /= 2;
+            u /= 2;
+
+            assert(u > 0);
+            real new_x[n / (2*u)];
+            int idx = 0;
+            for (i = capital_i - 1; i < n; i += 2*u) {
+                new_x[idx] = (rhs[i] - a[i]*x[i-u] - c[i]*x[i+u]) / diag[i];
+                idx += 1;
+            }
+
+            idx = 0;
+            for (i = capital_i - 1; i < n; i += 2*u) {
+                x[i] = new_x[idx];
+                idx += 1;
+            }
+
+            j += 1;
         }
     }
 }
 
+/**
+ * @brief      TPR STAGE 3
+ *
+ * @param[in]  st     start index of equation that this function calculate
+ * @param[in]  ed     end index of equation that this function calculate
+ */
+void TPR::tpr_stage3(int st, int ed) {
+    // REPLACING
+    for (int i = st; i <= ed; i += 1) {
+        if (i % 2 == 0) {
+            replace_with_init(i);
+        } else {
+            replace_with_st1(i);
+        }
+    }
 
-void TPR::tpr_stage1(int st, int ed) {}
+    int capital_i = s;
+    int u = s;
+    for (int j = 0; j < fllog2(s); j += 1) {
+    // while (u >= 2) {
+        capital_i = capital_i / 2;
+        u = u / 2;
+
+        assert(u > 0);
+        assert(capital_i > 0);
+        real new_x[n / (2*u)];
+        int idx = 0;
+        for (int i = st + capital_i - 1; i <= ed; i += 2*u) {
+            // update x[i]
+            new_x[idx] = (rhs[i] - a[i] * x[i-u] - c[i]*x[i+u]) / diag[i];
+            idx += 1;
+        }
+
+        assert(idx <= n / (2*u));
+        for (int i = st + capital_i - 1, idx = 0; i <= ed; i += 2*u, idx++) {
+            x[i] = new_x[idx];
+        }
+    }
+}
 
 /**
  * @brief      reduction calculation at section
@@ -219,104 +301,35 @@ EquationInfo TPR::update_lower_no_check(int kl, int i) {
 
 /**
  * @brief      make backup equation for STAGE 3 use.
+ *
+ * @param[in]    st    start index of equation
+ * @param[in]    ed    end index of equation
  */
-void TPR::mk_bkup_init() {
-    bkup_cp(this->a, this->init_a, 0, this->n);
-    bkup_cp(this->diag, this->init_diag, 0, this->n);
-    bkup_cp(this->c, this->init_c, 0, this->n);
-    bkup_cp(this->rhs, this->init_rhs, 0, this->n);
+void TPR::mk_bkup_init(int st, int ed) {
+    int stidx = st;
+    bkup_cp(this->a, this->init_a, stidx, ed);
+    bkup_cp(this->diag, this->init_diag, stidx, ed);
+    bkup_cp(this->c, this->init_c, stidx, ed);
+    bkup_cp(this->rhs, this->init_rhs, stidx, ed);
 }
 
 /**
  * @brief      make backup equation for STAGE 3 use.
+ *
+ * @param[in]    st    start index of equation
+ * @param[in]    ed    end index of equation
  */
-void TPR::mk_bkup_st1() {
-    bkup_cp(this->a, this->st1_a, 1, this->n);
-    bkup_cp(this->diag, this->st1_diag, 1, this->n);
-    bkup_cp(this->c, this->st1_c, 1, this->n);
-    bkup_cp(this->rhs, this->st1_rhs, 1, this->n);    
+void TPR::mk_bkup_st1(int st, int ed) {
+    int stidx = st + 1;
+    bkup_cp(this->a, this->st1_a, stidx, ed);
+    bkup_cp(this->diag, this->st1_diag, stidx, ed);
+    bkup_cp(this->c, this->st1_c, stidx, ed);
+    bkup_cp(this->rhs, this->st1_rhs, stidx, ed);
 }
 
 void TPR::bkup_cp(real *src, real *dst, int st,int ed) {
     for (int i = st; i < ed; i += 2) {
         dst[i / 2] = src[i];
-    }
-}
-
-
-/**
- * @brief      TPR BACKWARD SUBSTITUTION
- */
-void TPR::tpr_backward() {
-    int capital_i = n / 2;
-    int m = n / s;
-
-    int u = capital_i;
-    int i = capital_i-1;
-
-    // STAGE 2 (continue)
-    {
-        real inv_det = 1.0 / (diag[i+u]*diag[i] - c[i]*a[i+u]);
-        
-        x[i] = (diag[i+u]*rhs[i] - c[i]*rhs[i+u]) * inv_det;
-        x[i+u] =  (rhs[i+u]*diag[i] - rhs[i]*a[i+u]) * inv_det;
-    }    
-
-    int j = 0;
-    while (j < fllog2(m) - 1) {
-        capital_i /= 2;
-        u /= 2;
-    
-        assert(u > 0);
-        real new_x[n / 2*u];
-        int idx = 0;
-        for (i = capital_i - 1; i < n; i += 2*u) {
-            new_x[idx] = (rhs[i] - a[i]*x[i-u] - c[i]*x[i+u]) / diag[i];
-            idx += 1;
-        }
-
-        idx = 0;
-        for (i = capital_i - 1; i < n; i += 2*u) {
-            x[i] = new_x[idx];
-            idx += 1;
-        }
-
-        j += 1;
-    }
-    
-    for (i = 0; i < n; i += 1) {
-        if (i % 2 == 0) {
-            replace_with_init(i);
-        } else {
-            replace_with_st1(i);
-        }
-    }
-
-
-    // STAGE 3
-    // for (j = 0; j < fllog2(s); j += 1) {
-    while (u >= 2) {
-        capital_i = capital_i / 2;
-        u = u / 2;
-        int p = 0;
-
-        assert(u > 0);
-        assert(capital_i > 0);
-        while (p < n) {
-            real new_x[n / 2*u];
-            int idx = 0;
-            for (i = p + capital_i - 1; i < p + s; i += 2*u) {
-                // update x[i]
-                new_x[idx] = (rhs[i] - a[i] * x[i-u] - c[i]*x[i+u]) / diag[i];
-                idx += 1;
-            }
-
-            for (i = p + capital_i - 1, idx = 0; i < p + s; i += 2*u, idx++) {
-                x[i] = new_x[idx];
-            }
-
-            p += s;
-        }
     }
 }
 
