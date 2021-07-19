3ff57f0d739b11d236d6789c46824cd4a81ece73
TM-MT <tm-east@outlook.com>
2021-07-19 10:01:22 +0000

    Update PCR

--- src/pcr.cpp
+++ src/pcr.cpp
@@ -14,9 +14,16 @@ int PCR::solve() {
         int s = 1 << p;
         real a1[n], c1[n], rhs1[n];
 
+#ifdef _OPENMP
         #pragma omp parallel shared(a1, c1, rhs1, s)
+#endif
         {
+#ifdef _OPENACC
+#pragma acc loop vector
+#endif
+#ifdef _OPENMP
             #pragma omp for
+#endif
             for (int k = 0; k < n; k++) {
                 int kl = max(k-s, 0);
                 int kr = min(k+s, n-1);
@@ -31,7 +38,12 @@ int PCR::solve() {
                 rhs1[k] = e * ( rhs[k] - ap * rhs[kl] - cp * rhs[kr]);
             }
 
+#ifdef _OPENACC
+#pragma acc loop vector
+#endif
+#ifdef _OPENMP
             #pragma omp for
+#endif
             for (int k = 0; k < n; k++) {
                 a[k] = a1[k];
                 c[k] = c1[k];
