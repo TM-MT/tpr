131a01114b75ac0a19b62673c3ec67588f397df2
TM-MT <tm-east@outlook.com>
2021-07-29 14:03:28 +0900

    Update PCR  - remove not worth SIMD instructions  - split reduction for SIMD

--- src/pcr.cpp
+++ src/pcr.cpp
@@ -16,10 +16,24 @@ int PCR::solve() {
 
         #pragma omp parallel shared(a1, c1, rhs1, s)
         {
-            #pragma omp for
-            for (int k = 0; k < n; k++) {
-                int kl = max(k-s, 0);
-                int kr = min(k+s, n-1);
+            #pragma omp for schedule(static)
+            for (int k = 0; k < s; k++) {
+                int kr = k + s;
+
+                real ap = a[k];
+                real cp = c[k];
+
+                real e = 1.0 / ( 1.0 - cp * a[kr] );
+
+                a1[k] = e * ap;
+                c1[k] = -e * cp * c[kr];
+                rhs1[k] = e * ( rhs[k] - cp * rhs[kr]);
+            }
+
+            #pragma omp for schedule(static)
+            for (int k = s; k < n - s; k++) {
+                int kl = k - s;
+                int kr = k + s;
 
                 real ap = a[k];
                 real cp = c[k];
@@ -31,7 +45,20 @@ int PCR::solve() {
                 rhs1[k] = e * ( rhs[k] - ap * rhs[kl] - cp * rhs[kr]);
             }
 
-            #pragma omp for
+            #pragma omp for schedule(static)
+            for (int k = n - s; k < n; k++) {
+                int kl = k - s;
+
+                real ap = a[k];
+                real cp = c[k];
+
+                real e = 1.0 / ( 1.0 - ap * c[kl] );
+
+                a1[k] = -e * ap * a[kl];
+                c1[k] = e * cp;
+                rhs1[k] = e * ( rhs[k] - ap * rhs[kl]);
+            }
+
             for (int k = 0; k < n; k++) {
                 a[k] = a1[k];
                 c[k] = c1[k];
