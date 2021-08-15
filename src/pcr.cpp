2a643e51e26da0955ec315b4dcd1f955d09f5e01
TM-MT <tm-east@outlook.com>
2021-08-15 19:55:15 +0900

    remove if statements in PCR

--- src/pcr.cpp
+++ src/pcr.cpp
@@ -12,7 +12,7 @@ int PCR::solve() {
     real a1[n], c1[n], rhs1[n];
 
     for (int p = 0; p < pn; p++) {
-        #pragma acc kernels create(a1[:n], c1[:n], rhs1[:n]) present(this->a[:n], this->c[:n], this->rhs[:n], this, this->n)
+        #pragma acc kernels create(a1[:n], c1[:n], rhs1[:n]) present(this->a[-margin:n+margin], this->c[-margin:n+margin], this->rhs[-margin:n+margin], this, this->n)
         #ifdef _OPENMP
         #pragma omp parallel shared(a1, c1, rhs1)
         #endif
@@ -26,23 +26,7 @@ int PCR::solve() {
             #ifdef _OPENMP
             #pragma omp for schedule(static)
             #endif
-            for (int k = 0; k < s; k++) {
-                int kr = k + s;
-
-                real e = 1.0 / ( 1.0 - c[k] * a[kr]);
-
-                a1[k] = e * a[k];
-                c1[k] = -e * c[k] * c[kr];
-                rhs1[k] = e * (rhs[k] - c[k] * rhs[kr]);
-            }
-
-            #ifdef _OPENACC
-            #pragma acc loop independent
-            #endif
-            #ifdef _OPENMP
-            #pragma omp for schedule(static)
-            #endif
-            for (int k = s; k < n - s; k++) {
+            for (int k = 0; k < n; k++) {
                 int kl = k - s;
                 int kr = k + s;
 
@@ -54,25 +38,6 @@ int PCR::solve() {
                 a1[k] = -e * ap * a[kl];
                 c1[k] = -e * cp * c[kr];
                 rhs1[k] = e * ( rhs[k] - ap * rhs[kl] - cp * rhs[kr]);
-            }
-
-            #ifdef _OPENACC
-            #pragma acc loop independent
-            #endif
-            #ifdef _OPENMP
-            #pragma omp for schedule(static)
-            #endif
-            for (int k = n - s; k < n; k++) {
-                int kl = k - s;
-
-                real ap = a[k];
-                real cp = c[k];
-
-                real e = 1.0 / ( 1.0 - ap * c[kl] );
-
-                a1[k] = -e * ap * a[kl];
-                c1[k] = e * cp;
-                rhs1[k] = e * ( rhs[k] - ap * rhs[kl]);
             }
 
             #pragma acc loop
@@ -102,4 +67,26 @@ int PCR::get_ans(real *x) {
     }
     return 0;
 };
+
+real* PCR::extend_input_array(real *p, int len) {
+    int margin = array_margin(len);
+    real* start_point = extend_array(p, len, len + 2 * margin, margin);
+    return &start_point[margin];
+}
+
+real* PCR::extend_array(real *p, int oldlen, int newlen, int margin) {
+    real *ret = new real[newlen];
+
+    // fill 0
+    for (int i = 0; i < newlen; i++) {
+        ret[i] = 0.0;
+    }
+
+    // copy
+    for (int i = 0; i < oldlen; i++) {
+        ret[margin + i] = p[i];
+    }
+
+    return ret;
+}
 
--- src/pcr.hpp
+++ src/pcr.hpp
@@ -5,30 +5,42 @@ class PCR: Solver
 {
     real *a, *c, *rhs;
     int n;
+    int margin;
 
 public:
     PCR(real *a, real *diag, real *c, real *rhs, int n) {
-        this->a = a;
-        this->c = c;
-        this->rhs = rhs;
+        this->a = extend_input_array(a, n);
+        this->c = extend_input_array(c, n);
+        this->rhs = extend_input_array(rhs, n);
         this->n = n;
+        this->margin = array_margin(n);
 
-        #pragma acc enter data create(this, this->n)
-        #pragma acc enter data copyin(this->a[0:n], this->c[0:n], this->rhs[0:n]) wait
+        #pragma acc enter data create(this, this->n, this->margin)
+        #pragma acc enter data copyin(this->a[-margin:n+margin], this->c[-margin:n+margin], this->rhs[-margin:n+margin]) wait
         // TO-DO
         // make sure diag = {1., 1., ..., 1.};
     };
 
     ~PCR() {
-        #pragma acc exit data copyout(this->a[0:n], this->c[0:n], this->rhs[0:n])
-        #pragma acc exit data delete(this, this->n)
+        #pragma acc exit data delete(this->a[-margin:n+margin], this->c[-margin:n+margin], this->rhs[-margin:n+margin])
+        #pragma acc exit data delete(this, this->n, this->margin)
+        delete &this->a[-this->margin];
+        delete &this->c[-this->margin];
+        delete &this->rhs[-this->margin];
     }
  
     int solve();
 
     int get_ans(real *x);
 
+    real* extend_input_array(real *p, int len);
+
 private:
     PCR(const PCR &pcr);
     PCR &operator=(const PCR &pcr);
+
+    real* extend_array(real *p, int oldlen, int newlen, int margin);
+    inline int array_margin(int n) {
+        return 1 << fllog2(n);
+    }
 };
