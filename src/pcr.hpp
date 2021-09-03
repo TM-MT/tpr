9ed9da08abd2b896221934a31c7bff00ead2b999
TM-MT <tm-east@outlook.com>
2021-09-04 01:37:55 +0900

    Optimize data locality for PCR

--- src/pcr.hpp
+++ src/pcr.hpp
@@ -4,6 +4,7 @@
 class PCR: Solver
 {
     real *a, *c, *rhs;
+    real *a1, *c1, *rhs1;
     int n;
 
 public:
@@ -13,13 +14,24 @@ public:
         this->rhs = rhs;
         this->n = n;
 
+        this->a1 = new real[n];
+        this->c1 = new real[n];
+        this->rhs1 = new real[n];
+
         #pragma acc enter data copyin(this, this->n)
-        #pragma acc enter data copyin(this->a[0:n], this->c[0:n], this->rhs[0:n]) wait
+        #pragma acc enter data copyin(this->a[0:n], this->c[0:n], this->rhs[0:n])
+        #pragma acc enter data create(a1[:n], c1[:n], rhs1[:n]) 
+
         // TO-DO
         // make sure diag = {1., 1., ..., 1.};
     };
 
     ~PCR() {
+        delete[] this->a1;
+        delete[] this->c1;
+        delete[] this->rhs1;
+
+        #pragma acc exit data delete(a1[:n], c1[:n], rhs1[:n])
         #pragma acc exit data copyout(this->a[0:n], this->c[0:n], this->rhs[0:n])
         #pragma acc exit data delete(this, this->n)
     }
