fa25da2fc657241a3445fe2373191eed288a7c48
TM-MT <tm-east@outlook.com>
2021-09-16 11:26:38 +0900

    Update tpr_cu  - fix miss use of array  - add stage 2 and stage 3
    TODOs
     - block sync

--- src/tpr.cu
+++ src/tpr.cu
@@ -1,6 +1,5 @@
 #include "tpr.cuh"
 #include <iostream>
-#define SM_SIZE 256
 
 
 __global__ void tpr_ker(float *a, float *c, float *rhs, float *x, int n, int s) {
@@ -8,23 +7,14 @@ __global__ void tpr_ker(float *a, float *c, float *rhs, float *x, int n, int s) 
     int m = n / s;
     int st = idx / s * s;
     int ed = st + s - 1;
+    // printf("%d: %d, %d\n", idx, st, ed);
 
     float tmp_aa, tmp_cc, tmp_rr;
-    float *inter_a, *inter_c, *inter_rhs;
-    float *st2_a, *st2_c, *st2_rhs;
-        // if ((inter_a == NULL) || (inter_c == NULL) || (inter_rhs == NULL)
-        //     (st2_a == NULL) || (st2_c == NULL) || (st2_rhs == NULL)) {
-        // }
+    float inter_ast, inter_cst, inter_rhsst; // bkup
+    float inter_aed, inter_ced, inter_rhsed; // bkup
 
+    // stage 1
     if (idx < n) {
-        inter_a = (float*)malloc(2 * m * sizeof(float));
-        inter_c = (float*)malloc(2 * m * sizeof(float));
-        inter_rhs = (float*)malloc(2 * m * sizeof(float));
-        st2_a = (float *)malloc(m * sizeof(float));
-        st2_c = (float *)malloc(m * sizeof(float));
-        st2_rhs = (float *)malloc(m * sizeof(float));
-
-
         for (int p = 1; p <= static_cast<int>(log2f(static_cast<double>(s))); p++) {
             // reduction
             int u = 1 << (p - 1); // offset
@@ -66,50 +56,61 @@ __global__ void tpr_ker(float *a, float *c, float *rhs, float *x, int n, int s) 
 
             __syncthreads();
         }
+
+        // Update E_{st} by E_{ed}
+        if (idx == st) {
+            int k = st, kr = ed;
+            float ak = a[k], akr = a[kr];
+            float ck = c[k], ckr = c[kr];
+            float rhsk = rhs[k], rhskr = rhs[kr];
+
+            float inv_diag_k = 1.0 / (1.0 - akr * ck);
+
+            tmp_aa = inv_diag_k * ak;
+            tmp_cc = -inv_diag_k * ckr * ck;
+            tmp_rr = inv_diag_k * (rhsk - rhskr * ck);
+
+            inter_ast = a[st];
+            inter_cst = c[st];
+            inter_rhsst = rhs[st];
+
+            __syncthreads();
+
+            a[st] = tmp_aa;
+            c[st] = tmp_cc;
+            rhs[st] = tmp_rr;
+        }
+        __syncthreads();
     }
 
-    if (idx == st) {
-        int k = st, kr = ed;
-        int eqi_dst = 2 * st / s;
-        float ak = a[k];
-        float akr = a[kr];
-        float ck = c[k];
-        float ckr = c[kr];
-        float rhsk = rhs[k];
-        float rhskr = rhs[kr];
 
+    // Update E_{st-1} by E_{st}
+    if (idx < n - 1 && idx == ed) {
+        int k = idx, kr = idx+1; // (k, kr) = (st-1, st)
+        float ak = a[k], akr = a[kr];
+        float ck = c[k], ckr = c[kr];
+        float rhsk = rhs[k], rhskr = rhs[kr];
         float inv_diag_k = 1.0 / (1.0 - akr * ck);
 
-        inter_a[eqi_dst] = inv_diag_k * ak;
-        inter_c[eqi_dst] = -inv_diag_k * ckr * ck;
-        inter_rhs[eqi_dst] = inv_diag_k * (rhsk - rhskr * ck);
+        inter_aed = a[idx];
+        inter_ced = c[idx];
+        inter_rhsed = rhs[idx];
 
-        // Copy E_{ed}
-        inter_a[eqi_dst + 1] = akr; // a.k.a. a[ed]
-        inter_c[eqi_dst + 1] = ckr;
-        inter_rhs[eqi_dst + 1] = rhskr;
+        a[k] = inv_diag_k * ak;
+        c[k] = -inv_diag_k * ckr * ck;
+        rhs[k] = inv_diag_k * (rhsk - rhskr * ck);
     }
+
+    // FIX-ME should be block sync
     __syncthreads();
 
-    if (1 <= idx && idx < 2 * m - 1 && idx % 2 == 1) {
-        float ak = inter_a[idx];
-        float akr = inter_a[idx + 1];
-        float ck = inter_c[idx];
-        float ckr = inter_c[idx + 1];
-        float rhsk = inter_rhs[idx];
-        float rhskr = inter_rhs[idx + 1];
-        float inv_diag_k = 1.0 / (1.0 - akr * ck);
-
-        int dst = idx / 2;
-        st2_a[dst] = inv_diag_k * ak;
-        st2_c[dst] = -inv_diag_k * ckr * ck;
-        st2_rhs[dst] = inv_diag_k * (rhsk - rhskr * ck);        
-    }
-    __syncthreads();
 
     // PCR
     if (idx < m) {
-        for (int p = 1; p <= static_cast<int>(log2f(static_cast<double>(m))); p++) {
+        for (int p = static_cast<int>(log2f(static_cast<double>(s))) + 1;
+             p <= static_cast<int>(log2f(static_cast<double>(n))); 
+             p++) 
+        {
             // reduction
             int u = 1 << (p - 1); // offset
             int lidx = idx - u;
@@ -119,9 +120,9 @@ __global__ void tpr_ker(float *a, float *c, float *rhs, float *x, int n, int s) 
                 ckl = 0.0;
                 rkl = 0.0;
             } else {
-                akl = st2_a[lidx];
-                ckl = st2_c[lidx];
-                rkl = st2_rhs[lidx];
+                akl = a[lidx];
+                ckl = c[lidx];
+                rkl = rhs[lidx];
             }
             int ridx = idx + u;
             float akr, ckr, rkr;
@@ -130,34 +131,52 @@ __global__ void tpr_ker(float *a, float *c, float *rhs, float *x, int n, int s) 
                 ckr = -1.0;
                 rkr = 0.0;
             } else {
-                akr = st2_a[ridx];
-                ckr = st2_c[ridx];
-                rkr = st2_rhs[ridx];
+                akr = a[ridx];
+                ckr = c[ridx];
+                rkr = rhs[ridx];
             }
 
-            float inv_diag_k = 1.0 / (1.0 - ckl * st2_a[idx] - akr * st2_c[idx]);
+            float inv_diag_k = 1.0 / (1.0 - ckl * a[idx] - akr * c[idx]);
 
-            tmp_aa = - inv_diag_k * akl* st2_a[idx];
-            tmp_cc = - inv_diag_k * ckr * st2_c[idx];
-            tmp_rr = inv_diag_k * (st2_rhs[idx] - rkl * st2_a[idx] - rkr * st2_c[idx]);
+            tmp_aa = - inv_diag_k * akl* a[idx];
+            tmp_cc = - inv_diag_k * ckr * c[idx];
+            tmp_rr = inv_diag_k * (rhs[idx] - rkl * a[idx] - rkr * c[idx]);
 
             __syncthreads();
 
             // copy back
-            st2_a[idx] = tmp_aa;
-            st2_c[idx] = tmp_cc;
-            st2_rhs[idx] = tmp_rr;
+            a[idx] = tmp_aa;
+            c[idx] = tmp_cc;
+            rhs[idx] = tmp_rr;
 
             __syncthreads();
         }
     }
 
+    // FIX-ME should be block sync
+
+    // stage 3
     if (idx < n) {
-        // for (int i = s - 1; i < n; i += s) {
-        // copy the answer
-        if (idx % s == s - 1) {
-           x[idx] = st2_rhs[idx / s];
+        // copy the answer from stage 2 PCR
+        if (idx == ed) {
+            x[idx] = rhs[idx];
         }
+
+        // FIX-ME should be block sync
+        __syncthreads();
+
+        if (idx == st) {
+            a[idx] = inter_ast;
+            c[idx] = inter_cst;
+            rhs[idx] = inter_rhsst;
+        }
+
+        if (idx == ed && idx != n-1) {
+            a[idx] = inter_aed;
+            c[idx] = inter_ced;
+            rhs[idx] = inter_rhsed;            
+        }
+
         __syncthreads();
 
         int lidx = max(0, st - 1);
@@ -169,9 +188,7 @@ __global__ void tpr_ker(float *a, float *c, float *rhs, float *x, int n, int s) 
 
         x[idx] = rhs[idx] - a[idx] * x[lidx] - c[idx] * key;
     }
-    __syncthreads();
 }
-
 
 /**
 __global__ void pcr_ker(float *a, float *c, float *rhs, int n) {
