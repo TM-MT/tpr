9fdf14e7d70a1722a2ab968fec9b0d9a88e69450
TM-MT <tm-east@outlook.com>
2021-10-08 17:20:23 +0900

    Update pm.cu and gtest

--- test/cuexamples.cpp
+++ test/cuexamples.cpp
@@ -1,13 +1,15 @@
 #include <gtest/gtest.h>
 
+#include "lib.hpp"
 #include "main.hpp"
 #include "ptpr.cuh"
+#include "system.hpp"
 #include "tpr.cuh"
 
 class CuExamples : public ::testing::Test {
    public:
-    struct TRIDIAG_SYSTEM *sys = nullptr;
     const static int n = 1024;
+    trisys::ExampleFixedInput *input;
 
     real ans_array[n] = {
 #include "ans1024.txt"
@@ -15,16 +17,11 @@ class CuExamples : public ::testing::Test {
 
    protected:
     void SetUp() override {
-        sys = (struct TRIDIAG_SYSTEM *)malloc(sizeof(struct TRIDIAG_SYSTEM));
-        setup(sys, n);
-
-        assign(sys);
+        input = new trisys::ExampleFixedInput(n);
+        input->assign();
     }
 
-    void TearDown() override {
-        clean(sys);
-        free(sys);
-    }
+    void TearDown() override { delete input; }
 
     void array_float_eq(real *expect, real *actual) {
         for (int i = 0; i < n; i++) {
@@ -50,79 +47,37 @@ class CuExamples : public ::testing::Test {
 };
 
 TEST_F(CuExamples, CRTest) {
-    TPR_CU::cr_cu(sys->a, sys->c, sys->rhs, sys->diag, n);
-    array_float_eq(ans_array, sys->diag);
-    array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+    TPR_CU::cr_cu(input->sys.a, input->sys.c, input->sys.rhs, input->sys.diag,
+                  n);
+    array_float_eq(ans_array, input->sys.diag);
+    array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
 
 TEST_F(CuExamples, PCRTest) {
-    PTPR_CU::pcr_cu(sys->a, sys->c, sys->rhs, sys->diag, n);
-    array_float_eq(ans_array, sys->diag);
-    array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+    PTPR_CU::pcr_cu(input->sys.a, input->sys.c, input->sys.rhs, input->sys.diag,
+                    n);
+    array_float_eq(ans_array, input->sys.diag);
+    array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
 
 TEST_F(CuExamples, TPRTest) {
     for (int s = 64; s <= 1024; s *= 2) {
-        assign(sys);
-        TPR_CU::tpr_cu(sys->a, sys->c, sys->rhs, sys->diag, n, s);
-        print_array(sys->diag, n);
-        array_float_eq(ans_array, sys->diag);
-        array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+        input->assign();
+        TPR_CU::tpr_cu(input->sys.a, input->sys.c, input->sys.rhs,
+                       input->sys.diag, n, s);
+        print_array(input->sys.diag, n);
+        array_float_eq(ans_array, input->sys.diag);
+        array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
     }
 }
 
 TEST_F(CuExamples, PTPRTest) {
     for (int s = 64; s <= 1024; s *= 2) {
-        assign(sys);
-        PTPR_CU::ptpr_cu(sys->a, sys->c, sys->rhs, sys->diag, n, s);
-        print_array(sys->diag, n);
-        array_float_eq(ans_array, sys->diag);
-        array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+        input->assign();
+        PTPR_CU::ptpr_cu(input->sys.a, input->sys.c, input->sys.rhs,
+                         input->sys.diag, n, s);
+        print_array(input->sys.diag, n);
+        array_float_eq(ans_array, input->sys.diag);
+        array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
     }
-}
-
-int setup(struct TRIDIAG_SYSTEM *sys, int n) {
-    sys->a = (real *)malloc(n * sizeof(real));
-    sys->diag = (real *)malloc(n * sizeof(real));
-    sys->c = (real *)malloc(n * sizeof(real));
-    sys->rhs = (real *)malloc(n * sizeof(real));
-    sys->n = n;
-
-    return sys_null_check(sys);
-}
-
-int assign(struct TRIDIAG_SYSTEM *sys) {
-    int n = sys->n;
-    for (int i = 0; i < n; i++) {
-        sys->a[i] = -1.0 / 6.0;
-        sys->c[i] = -1.0 / 6.0;
-        sys->diag[i] = 1.0;
-        sys->rhs[i] = 1.0 * (i + 1);
-    }
-    sys->a[0] = 0.0;
-    sys->c[n - 1] = 0.0;
-
-    return 0;
-}
-
-int clean(struct TRIDIAG_SYSTEM *sys) {
-    for (auto p : {sys->a, sys->diag, sys->c, sys->rhs}) {
-        free(p);
-    }
-
-    sys->a = nullptr;
-    sys->diag = nullptr;
-    sys->c = nullptr;
-    sys->rhs = nullptr;
-
-    return 0;
-}
-
-bool sys_null_check(struct TRIDIAG_SYSTEM *sys) {
-    for (auto p : {sys->a, sys->diag, sys->c, sys->rhs}) {
-        if (p == nullptr) {
-            return false;
-        }
-    }
-    return true;
 }
--- test/examples.cpp
+++ test/examples.cpp
@@ -7,14 +7,15 @@
 #include "pcr.hpp"
 #include "pm.hpp"
 #include "ptpr.hpp"
+#include "system.hpp"
 #include "tpr.hpp"
 
 pm_lib::PerfMonitor pmcpp::pm = pm_lib::PerfMonitor();
 
 class Examples : public ::testing::Test {
    public:
-    struct TRIDIAG_SYSTEM *sys = nullptr;
     const static int n = 1024;
+    trisys::ExampleInput *input = nullptr;
 
     real ans_array[n] = {
 #include "ans1024.txt"
@@ -22,16 +23,11 @@ class Examples : public ::testing::Test {
 
    protected:
     void SetUp() override {
-        sys = (struct TRIDIAG_SYSTEM *)malloc(sizeof(struct TRIDIAG_SYSTEM));
-        setup(sys, n);
-
-        assign(sys);
+        input = new trisys::ExampleFixedInput(n);
+        input->assign();
     }
 
-    void TearDown() override {
-        clean(sys);
-        free(sys);
-    }
+    void TearDown() override { delete input; }
 
     void array_float_eq(real *expect, real *actual) {
         for (int i = 0; i < n; i++) {
@@ -59,101 +55,58 @@ class Examples : public ::testing::Test {
 TEST_F(Examples, CRTest) {
 #pragma acc data copy(sys->a[:n], sys->c[:n], sys->rhs[:n], sys->n)
     {
-        CR cr(sys->a, sys->diag, sys->c, sys->rhs, sys->n);
+        CR cr(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs, n);
         cr.solve();
-        cr.get_ans(sys->diag);
+        cr.get_ans(input->sys.diag);
     }
-    array_float_eq(ans_array, sys->diag);
-    array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+    array_float_eq(ans_array, input->sys.diag);
+    array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
 
 TEST_F(Examples, PCRTest) {
 #pragma acc data copy(sys->a[:n], sys->c[:n], sys->rhs[:n], sys->n)
     {
-        PCR pcr(sys->a, sys->diag, sys->c, sys->rhs, sys->n);
+        PCR pcr(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
+                input->sys.n);
         pcr.solve();
-        pcr.get_ans(sys->diag);
+        pcr.get_ans(input->sys.diag);
     }
-    array_float_eq(ans_array, sys->diag);
-    array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+    array_float_eq(ans_array, input->sys.diag);
+    array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
 
 TEST_F(Examples, TPRTest) {
     for (int s = 4; s <= n; s *= 2) {
-        assign(sys);
+        input->assign();
 #pragma acc data copy( \
     sys->a[:n], sys->diag[:n], sys->c[:n], sys->rhs[:n], sys->n)
         {
-            TPR t(sys->a, sys->diag, sys->c, sys->rhs, sys->n, s);
+            TPR t(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
+                  input->sys.n, s);
             t.solve();
-            t.get_ans(sys->diag);
+            t.get_ans(input->sys.diag);
         }
-        print_array(sys->diag, n);
+        print_array(input->sys.diag, n);
         printf("\n");
     }
-    array_float_eq(ans_array, sys->diag);
-    array_float_maxsqsum(ans_array, sys->diag, 1e-3);
+    array_float_eq(ans_array, input->sys.diag);
+    array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
 
 TEST_F(Examples, PTPRTest) {
     for (int s = 4; s <= n; s *= 2) {
-        assign(sys);
+        input->assign();
 #pragma acc data copy( \
     sys->a[:n], sys->diag[:n], sys->c[:n], sys->rhs[:n], sys->n)
         {
-            PTPR t(sys->a, sys->diag, sys->c, sys->rhs, sys->n, s);
+            PTPR t(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
+                   input->sys.n, s);
             t.solve();
-            t.get_ans(sys->diag);
+            t.get_ans(input->sys.diag);
         }
-        print_array(sys->diag, n);
+        print_array(input->sys.diag, n);
         printf("\n");
     }
-    array_float_eq(ans_array, sys->diag);
-    array_float_maxsqsum(ans_array, sys->diag, 1e-3);
-}
-
-int setup(struct TRIDIAG_SYSTEM *sys, int n) {
-    sys->a = (real *)malloc(n * sizeof(real));
-    sys->diag = (real *)malloc(n * sizeof(real));
-    sys->c = (real *)malloc(n * sizeof(real));
-    sys->rhs = (real *)malloc(n * sizeof(real));
-    sys->n = n;
-
-    return sys_null_check(sys);
-}
-
-int assign(struct TRIDIAG_SYSTEM *sys) {
-    int n = sys->n;
-    for (int i = 0; i < n; i++) {
-        sys->a[i] = -1.0 / 6.0;
-        sys->c[i] = -1.0 / 6.0;
-        sys->diag[i] = 1.0;
-        sys->rhs[i] = 1.0 * (i + 1);
-    }
-    sys->a[0] = 0.0;
-    sys->c[n - 1] = 0.0;
-
-    return 0;
-}
-
-int clean(struct TRIDIAG_SYSTEM *sys) {
-    for (auto p : {sys->a, sys->diag, sys->c, sys->rhs}) {
-        free(p);
-    }
-
-    sys->a = nullptr;
-    sys->diag = nullptr;
-    sys->c = nullptr;
-    sys->rhs = nullptr;
-
-    return 0;
-}
-
-bool sys_null_check(struct TRIDIAG_SYSTEM *sys) {
-    for (auto p : {sys->a, sys->diag, sys->c, sys->rhs}) {
-        if (p == nullptr) {
-            return false;
-        }
-    }
-    return true;
+    array_float_eq(ans_array, input->sys.diag);
+    array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
