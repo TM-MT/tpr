816be2042a9b8fa51fd2cccd5123e4d3b12e1d59
TM-MT <tm-east@outlook.com>
2021-11-10 14:16:23 +0900

    remove openACC directives from the project

--- test/examples.cpp
+++ test/examples.cpp
@@ -57,26 +57,18 @@ class Examples : public ::testing::Test {
 };
 
 TEST_F(Examples, CRTest) {
-#pragma acc data copy( \
-    input->sys.a[:n], input->sys.c[:n], input->sys.rhs[:n], input->sys.n)
-    {
-        CR cr(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs, n);
-        cr.solve();
-        cr.get_ans(input->sys.diag);
-    }
+    CR cr(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs, n);
+    cr.solve();
+    cr.get_ans(input->sys.diag);
     array_float_eq(ans_array, input->sys.diag);
     array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
 
 TEST_F(Examples, PCRTest) {
-#pragma acc data copy( \
-    input->sys.a[:n], input->sys.c[:n], input->sys.rhs[:n], input->sys.n)
-    {
-        PCR pcr(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
-                input->sys.n);
-        pcr.solve();
-        pcr.get_ans(input->sys.diag);
-    }
+    PCR pcr(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
+            input->sys.n);
+    pcr.solve();
+    pcr.get_ans(input->sys.diag);
     array_float_eq(ans_array, input->sys.diag);
     array_float_maxsqsum(ans_array, input->sys.diag, 1e-3);
 }
@@ -84,15 +76,10 @@ TEST_F(Examples, PCRTest) {
 TEST_F(Examples, TPRTest) {
     for (int s = 4; s <= n; s *= 2) {
         input->assign();
-#pragma acc data copy(                                                      \
-    input->sys.a[:n], input->sys.diag[:n], input->sys.c[:n], input->sys.rhs \
-    [:n], input->sys.n)
-        {
-            TPR t(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
-                  input->sys.n, s);
-            t.solve();
-            t.get_ans(input->sys.diag);
-        }
+        TPR t(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
+              input->sys.n, s);
+        t.solve();
+        t.get_ans(input->sys.diag);
         print_array(input->sys.diag, n);
         printf("\n");
     }
@@ -103,15 +90,10 @@ TEST_F(Examples, TPRTest) {
 TEST_F(Examples, PTPRTest) {
     for (int s = 4; s <= n; s *= 2) {
         input->assign();
-#pragma acc data copy(                                                      \
-    input->sys.a[:n], input->sys.diag[:n], input->sys.c[:n], input->sys.rhs \
-    [:n], input->sys.n)
-        {
-            PTPR t(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
-                   input->sys.n, s);
-            t.solve();
-            t.get_ans(input->sys.diag);
-        }
+        PTPR t(input->sys.a, input->sys.diag, input->sys.c, input->sys.rhs,
+               input->sys.n, s);
+        t.solve();
+        t.get_ans(input->sys.diag);
         print_array(input->sys.diag, n);
         printf("\n");
     }
@@ -121,7 +103,6 @@ TEST_F(Examples, PTPRTest) {
 
 TEST_F(Examples, TPRMultiThreadTest) {
     omp_set_num_threads(nt);
-    real tpr_result[nt][n];
 
     for (int s = 4; s <= n; s *= 2) {
         TPR t(input->sys.n, s);
@@ -149,7 +130,6 @@ TEST_F(Examples, TPRMultiThreadTest) {
 
 TEST_F(Examples, PTPRMultiThreadTest) {
     omp_set_num_threads(nt);
-    real tpr_result[nt][n];
 
     for (int s = 4; s <= n; s *= 2) {
         PTPR t(input->sys.n, s);
