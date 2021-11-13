fe9fa6eab83af740e7fe8dd187f4e809fde8d259
TM-MT <tm-east@outlook.com>
2021-11-13 18:16:42 +0900

    Update PCR  - remove UB  - add functions

--- src/pcr.cpp
+++ src/pcr.cpp
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 
+#include <cstring>
+
 #include "lib.hpp"
 
 /**
@@ -17,27 +19,29 @@ int PCR::solve() {
 #pragma omp parallel shared(a1, c1, rhs1)
         {
 #pragma omp for schedule(static)
-            for (int k = 0; k < n; k++) {
+            for (int k = this->margin; k < this->margin + this->n; k++) {
                 int kl = k - s;
                 int kr = k + s;
-                assert(kl >= -this->margin);
-                assert(kr < this->n + this->margin);
+                assert(kl >= 0);
+                assert(kr < this->n + 2 * this->margin);
 
                 real ap = a[k];
                 real cp = c[k];
 
                 real e = 1.0 / (1.0 - ap * c[kl] - cp * a[kr]);
 
-                a1[k] = -e * ap * a[kl];
-                c1[k] = -e * cp * c[kr];
-                rhs1[k] = e * (rhs[k] - ap * rhs[kl] - cp * rhs[kr]);
+                int dst = k - this->margin;
+                a1[dst] = -e * ap * a[kl];
+                c1[dst] = -e * cp * c[kr];
+                rhs1[dst] = e * (rhs[k] - ap * rhs[kl] - cp * rhs[kr]);
             }
 
 #pragma omp for schedule(static)
             for (int k = 0; k < n; k++) {
-                a[k] = a1[k];
-                c[k] = c1[k];
-                rhs[k] = rhs1[k];
+                int dst = k + this->margin;
+                a[dst] = a1[k];
+                c[dst] = c1[k];
+                rhs[dst] = rhs1[k];
             }
         }
     }
@@ -52,29 +56,53 @@ int PCR::solve() {
  */
 int PCR::get_ans(real *x) {
     for (int i = 0; i < n; i++) {
-        x[i] = this->rhs[i];
+        x[i] = this->rhs[i + margin];
     }
     return 0;
 };
 
 real *PCR::extend_input_array(real *p, int len) {
+    real *ret = create_extend_array(len);
     int margin = array_margin(len);
-    real *start_point = extend_array(p, len, len + 2 * margin, margin);
-    return &start_point[margin];
+
+    for (int i = 0; i < margin; i++) {
+        ret[i] = 0.0;
+        ret[2 * margin + n - i - 1] = 0.0;
+    }
+    std::memcpy(&ret[margin], p, len * sizeof(real));
+
+    return ret;
 }
 
-real *PCR::extend_array(real *p, int oldlen, int newlen, int margin) {
-    real *ret = new real[newlen];
+void PCR::free_extend_input_array(real *p, int len) { delete[] p; }
 
-    // fill 0
+/**
+ * @brief      Creates an extend array for PCR_ESA use.
+ *
+ * @param[in]  oldlen  The oldlen
+ *
+ * @return     pointer `p` s.t. `p` points to `p[0]` and
+ *             `p[-margin:oldlen+margin]`
+ */
+real *PCR::create_extend_array(int oldlen) {
+    int margin = array_margin(oldlen);
+    return create_extend_array(oldlen, margin);
+}
+
+/**
+ * @brief      Creates an extend array for PCR_ESA use.
+ *
+ * @param[in]  oldlen  The oldlen
+ * @param[in]  margin  The margin
+ *
+ * @return     pointer `p` s.t. `p` points to `p[0]` and
+ *             `p[-margin:oldlen+margin]`
+ */
+real *PCR::create_extend_array(int oldlen, int margin) {
+    int newlen = oldlen + 2 * margin;
+    real *ret = new real[newlen];
     for (int i = 0; i < newlen; i++) {
         ret[i] = 0.0;
     }
-
-    // copy
-    for (int i = 0; i < oldlen; i++) {
-        ret[margin + i] = p[i];
-    }
-
     return ret;
 }
--- src/pcr.hpp
+++ src/pcr.hpp
@@ -2,12 +2,22 @@
 #include "lib.hpp"
 
 class PCR : Solver {
+    /**
+     * a[0:n+2*margin]
+     * a: 0 ... 0 a[0] a[1] .... a[n-1] 0 ... 0
+     *    <-----> <------elements-----> <----->
+     *      |                             |
+     *      \- extra length of `margin`   \-  extra
+     */
     real *a, *c, *rhs;
     real *a1, *c1, *rhs1;
     int n;
-    int margin;
+    bool using_local_allocation =
+        false;  // `a, c, rhs` are allocated by PCR when true
 
    public:
+    int margin;
+
     PCR(real *a, real *diag, real *c, real *rhs, int n) {
         init(n);
         set_tridiagonal_system(a, diag, c, rhs);
@@ -16,9 +26,11 @@ class PCR : Solver {
     PCR(){};
 
     ~PCR() {
-        delete[] & this->a[-this->margin];
-        delete[] & this->c[-this->margin];
-        delete[] & this->rhs[-this->margin];
+        if (this->using_local_allocation) {
+            free_extend_input_array(a, this->n);
+            free_extend_input_array(c, this->n);
+            free_extend_input_array(rhs, this->n);
+        }
         delete[] this->a1;
         delete[] this->c1;
         delete[] this->rhs1;
@@ -39,18 +51,37 @@ class PCR : Solver {
     }
 
     void set_tridiagonal_system(real *a, real *diag, real *c, real *rhs) {
+        this->using_local_allocation = true;
         this->a = extend_input_array(a, n);
         this->c = extend_input_array(c, n);
         this->rhs = extend_input_array(rhs, n);
+    }
+
+    /**
+     * @brief      Sets the tridiagonal system.
+     *
+     * @param      a     a[0:n+2*margin], a[margin:margin+n] holds the
+     *                   subdiagonal elements of A.
+     * @param      diag  nullptr
+     * @param      c     c[0:n+2*margin], c[margin:margin+n] holds the
+     *                   superdiagonal elements of A.
+     * @param      rhs   rhs[0:n+2*margin], rhs[margin:margin+n] holds the
+     *                   right-hand-side of the equation.
+     */
+    void set_tridiagonal_system_no_extend(real *a, real *diag, real *c,
+                                          real *rhs) {
+        this->a = a;
+        this->c = c;
+        this->rhs = rhs;
     }
 
     int solve();
 
     int get_ans(real *x);
 
+    real *create_extend_array(int oldlen);
+    real *create_extend_array(int oldlen, int margin);
     real *extend_input_array(real *p, int len);
-
-   private:
-    real *extend_array(real *p, int oldlen, int newlen, int margin);
+    void free_extend_input_array(real *p, int len);
     inline int array_margin(int n) { return 1 << fllog2(n); }
 };
