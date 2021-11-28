501d7f8b670e58a57acdfe0f2f010943205175e6
TM-MT <tm-east@outlook.com>
2021-11-28 14:21:48 +0900

    Update gen_matrix  - added generating `xt`  - use `writematrix` function instead `save`  -

--- src/inputs/gen_matrix.m
+++ src/inputs/gen_matrix.m
@@ -27,28 +27,38 @@
 % 20. tridiag(-ones(N-1,1), 4*ones(N,1),c), c sampled from U (−1, 1)
 
 n = 512;
+
+% Define true as a normal distribution of floating-point numbers with a mean value of 3 and standard deviation of 1.
+xt = randn(n, 1) + 3;
+writematrix(xt);
+
 % keep condition number
 fid = fopen("cond.dat", "w");
 
 m1 = diag(rand(n - 1, 1) * 2 - 1, -1) + diag(rand(n, 1) * 2 - 1, 0) + diag(rand(n - 1, 1) * 2 - 1, 1);
+eq1 = [m1 m1*xt];
 fprintf(fid, "%e\n", cond(m1));
-save('m1.txt', 'm1', '-ascii');
+writematrix(eq1);
 
 m2 = m1;
 for i=1:n
     m2(i, i) = 1e8;
 end
+eq2 = [m2 m2*xt];
 fprintf(fid, "%e\n", cond(m2));
-save('m2.txt', 'm2', '-ascii');
+writematrix(eq2);
 
-m3 = [gallery("lesp", n), rand(n, 1) * 2 - 1];
+
+m3 = gallery("lesp", n);
+eq3 = [m3 m3*xt]
 fprintf(fid, "%e\n", cond(m3));
-save('m3.txt', 'm3', '-ascii');
+writematrix(eq3);
 
 m4 = m1;
 m4(n/2+1, n/2) = m4(n/2+1, n/2) * 1e-50;
+eq4 = [m4 m4*xt]
 fprintf(fid, "%e\n", cond(m4));
-save('m4.txt', 'm4', '-ascii');
+writematrix(eq4);
 
 m5 = m1;
 for i=1:n-1
@@ -61,76 +71,91 @@ for i=2:n
         m5(i - 1, i) = 0.0;
     end
 end
+eq5 = [m5 m5*xt]
 fprintf(fid, "%e\n", cond(m5));
-save('m5.txt', 'm5', '-ascii')
+writematrix(eq5)
 
 m6 = 64 * eye(n) + diag(rand(n - 1, 1) * 2 - 1, -1) + diag(rand(n - 1, 1) * 2 - 1, 1);
+eq6 = [m6 m6*xt]
 fprintf(fid, "%e\n", cond(m6));
-save('m6.txt', 'm6', '-ascii')
+writematrix(eq6)
 
 m7 = inv(gallery("kms", n, 0.5));
+eq7 = [m7 m7*xt]
 fprintf(fid, "%e\n", cond(m7));
-save('m7.txt', 'm7', '-ascii');
+writematrix(eq7);
 
 m8 = gallery("randsvd", n, 1e15, 2, 1, 1);
+eq8 = [m8 m8*xt]
 fprintf(fid, "%e\n", cond(m8));
-save('m8.txt', 'm8', '-ascii')
+writematrix(eq8)
 m9 = gallery("randsvd", n, 1e15, 3, 1, 1);
+eq9 = [m9 m9*xt]
 fprintf(fid, "%e\n", cond(m9));
-save('m9.txt', 'm9', '-ascii')
+writematrix(eq9)
 m10 = gallery("randsvd", n, 1e15, 1, 1, 1);
+eq10 = [m10 m10*xt]
 fprintf(fid, "%e\n", cond(m10));
-save('m10.txt', 'm10', '-ascii')
+writematrix(eq10)
 m11 = gallery("randsvd", n, 1e15, 4, 1, 1);
+eq11 = [m11 m11*xt]
 fprintf(fid, "%e\n", cond(m11));
-save('m11.txt', 'm11', '-ascii')
+writematrix(eq11)
 
 m12 = m1;
 for i=2:n
     m12(i - 1, i) = m12(i - 1, i) * 1e-50;
 end
+eq12 = [m12 m12*xt]
 fprintf(fid, "%e\n", cond(m12));
-save('m12.txt', 'm12', '-ascii')
+writematrix(eq12)
 
 m13 = gallery("dorr", n, 1e-4);
+eq13 = [m13 m13*xt]
 fprintf(fid, "%e\n", cond(m13));
-% save('m13.txt', 'm13', '-ascii')
-writematrix(m13)
+writematrix(eq13)
 
 % 14. tridiag(a, 1e-8*ones(N,1), c) with a,c sampled from U (−1, 1)
 m14 = diag(rand(n - 1, 1) * 2 - 1, -1) + 1e-8 * eye(n) + diag(rand(n - 1, 1) * 2 - 1, 1);
+eq14 = [m14 m14*xt]
 fprintf(fid, "%e\n", cond(m14));
-save('m14.txt', 'm14', '-ascii')
+writematrix(eq14)
 
 % 15. tridiag(a, zeros(N,1), c) with a,c sampled from U (−1, 1) 
 m15 = diag(rand(n - 1, 1) * 2 - 1, -1) + zeros(n) + diag(rand(n - 1, 1) * 2 - 1, 1);
+eq15 = [m15 m15*xt]
 fprintf(fid, "%e\n", cond(m15));
-save('m15.txt', 'm15', '-ascii')
+writematrix(eq15)
 
 % 16. tridiag(ones(N-1,1), 1e-8*ones(N,1),ones(N-1,1)) 
 m16 = diag(ones(n - 1, 1), -1) + 1e-8 * eye(n) + diag(ones(n - 1, 1), 1);
+eq16 = [m16 m16*xt]
 fprintf(fid, "%e\n", cond(m16));
-save('m16.txt', 'm16', '-ascii')
+writematrix(eq16)
 
 % 17. tridiag(ones(N-1,1), 1e8*ones(N,1),ones(N-1,1)) 
 m17 = diag(ones(n - 1, 1), -1) + 1e8 * eye(n) + diag(ones(n - 1, 1), 1);
+eq17 = [m17 m17*xt]
 fprintf(fid, "%e\n", cond(m17));
-save('m17.txt', 'm17', '-ascii')
+writematrix(eq17)
 
 % 18. tridiag(-ones(N-1,1), 4*ones(N,1),-ones(N-1,1)) 
 m18 = diag(-1 * ones(n - 1, 1), -1) + 4 * eye(n) + diag(-1 * ones(n - 1, 1), 1);
+eq18 = [m18 m18*xt]
 fprintf(fid, "%e\n", cond(m18));
-save('m18.txt', 'm18', '-ascii')
+writematrix(eq18)
 
 % 19. tridiag(-ones(N-1,1), 4*ones(N,1),ones(N-1,1)) 
 m19 = diag(-1 * ones(n - 1, 1), -1) + 4 * eye(n) + diag(1 * ones(n - 1, 1), 1);
+eq19 = [m19 m19*xt]
 fprintf(fid, "%e\n", cond(m19));
-save('m19.txt', 'm19', '-ascii')
+writematrix(eq19)
 
 % 20. tridiag(-ones(N-1,1), 4*ones(N,1),c), c sampled from U (−1, 1)
 m20 = diag(-1 * ones(n - 1, 1), -1) + 4 * eye(n) + diag(rand(n - 1, 1) * 2 - 1, 1);
+eq20 = [m20 m20*xt]
 fprintf(fid, "%e\n", cond(m20));
-save('m20.txt', 'm20', '-ascii')
+writematrix(eq20)
 
 fclose(fid);
 
